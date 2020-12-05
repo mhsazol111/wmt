900c5b35d4c3c9aeb1a38ceac313176c79f1427f
Mehedi Hassan Sazol <mhsazol111@gmail.com>
2020-12-06 04:01:41 +0600

    Gallery update and some fixing

--- src/Gallery.js
+++ src/Gallery.js
@@ -28,22 +28,61 @@ class Gallery extends Component {
           </div>
           <div id="amz-gallery-slider" className="amz-gallery-slider owl-carousel">
             <div className="amz-gallery-item">
-              <img src={`${themeUrl}/assets/images/gallery/Gallery-1.jpg`} alt="gallery1" />
+              <img src={`${themeUrl}/assets/images/gallery/gallery-1.jpg`} alt="gallery1" />
             </div>
             <div className="amz-gallery-item">
-              <img src={`${themeUrl}/assets/images/gallery/Gallery-2.jpg`} alt="gallery2" />
+              <img src={`${themeUrl}/assets/images/gallery/gallery-2.jpg`} alt="gallery2" />
             </div>
             <div className="amz-gallery-item">
-              <img src={`${themeUrl}/assets/images/gallery/Gallery-3.jpg`} alt="gallery3" />
+              <img src={`${themeUrl}/assets/images/gallery/gallery-3.jpg`} alt="gallery3" />
             </div>
             <div className="amz-gallery-item">
-              <img src={`${themeUrl}/assets/images/gallery/Gallery-4.jpg`} alt="gallery4" />
+              <img src={`${themeUrl}/assets/images/gallery/gallery-4.jpg`} alt="gallery4" />
             </div>
             <div className="amz-gallery-item">
-              <img src={`${themeUrl}/assets/images/gallery/Gallery-5.jpg`} alt="gallery5" />
+              <img src={`${themeUrl}/assets/images/gallery/gallery-5.jpg`} alt="gallery5" />
             </div>
             <div className="amz-gallery-item">
-              <img src={`${themeUrl}/assets/images/gallery/Gallery-6.jpg`} alt="gallery6" />
+              <img src={`${themeUrl}/assets/images/gallery/gallery-6.jpg`} alt="gallery6" />
+            </div>
+            <div className="amz-gallery-item">
+              <img src={`${themeUrl}/assets/images/gallery/gallery-7.jpg`} alt="gallery7" />
+            </div>
+            <div className="amz-gallery-item">
+              <img src={`${themeUrl}/assets/images/gallery/gallery-8.jpg`} alt="gallery8" />
+            </div>
+            <div className="amz-gallery-item">
+              <img src={`${themeUrl}/assets/images/gallery/gallery-9.jpg`} alt="gallery9" />
+            </div>
+            <div className="amz-gallery-item">
+              <img src={`${themeUrl}/assets/images/gallery/gallery-10.jpg`} alt="gallery10" />
+            </div>
+            <div className="amz-gallery-item">
+              <img src={`${themeUrl}/assets/images/gallery/gallery-11.jpg`} alt="gallery11" />
+            </div>
+            <div className="amz-gallery-item">
+              <img src={`${themeUrl}/assets/images/gallery/gallery-12.jpg`} alt="gallery12" />
+            </div>
+            <div className="amz-gallery-item">
+              <img src={`${themeUrl}/assets/images/gallery/gallery-13.jpg`} alt="gallery13" />
+            </div>
+            <div className="amz-gallery-item">
+              <img src={`${themeUrl}/assets/images/gallery/gallery-14.jpg`} alt="gallery14" />
+            </div>
+            <div className="amz-gallery-item">
+              <img src={`${themeUrl}/assets/images/gallery/gallery-15.jpg`} alt="gallery15" />
+            </div>
+            <div className="amz-gallery-item">
+              <img src={`${themeUrl}/assets/images/gallery/gallery-16.jpg`} alt="gallery16" />
+            </div>
+            <div className="amz-gallery-item">
+              <img src={`${themeUrl}/assets/images/gallery/gallery-17.jpg`} alt="gallery17" />
+            </div>
+            <div className="amz-gallery-item">
+              <img src={`${themeUrl}/assets/images/gallery/gallery-18.jpg`} alt="gallery18" />
+            </div>
+            <div className="amz-gallery-item">
+              <img src={`${themeUrl}/assets/images/gallery/gallery-19.jpg`} alt="gallery19" />
             </div>
           </div>
           <div id="wmt-nav" className="owl-nav"></div>
